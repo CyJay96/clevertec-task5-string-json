e4fa6574d26a5b1231481512fe1fe6f433749583
CyJay96 <konstantin201586@gmail.com>
2023-02-27 02:14:43 +0300

    feat: add converter from json

--- lib/src/main/java/ru/clevertec/JsonConverter.java
+++ lib/src/main/java/ru/clevertec/JsonConverter.java
@@ -1,7 +1,11 @@
 package ru.clevertec;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.lang.reflect.Array.get;
 import static java.lang.reflect.Array.getLength;
@@ -93,6 +97,47 @@ public class JsonConverter {
         } else {
             jsonArray.append(toJson(value)).append(",");
         }
+    }
+
+    public static <T> T fromJson(String json, Class<T> classType) {
+        Map<String, Object> fieldsValues = null;
+        try {
+            fieldsValues = new ObjectMapper().readValue(json, HashMap.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return setObjectFields(fieldsValues, classType);
+    }
+
+    private static <T> T setObjectFields(Map<String, Object> fieldsValues, Class<T> classType) {
+        T object = null;
+        try {
+            object = classType.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Field[] fields = classType.getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object value = fieldsValues.get(field.getName());
+            if (value != null) {
+                try {
+                    if (field.getType().equals(Character.class)) {
+                        field.set(object, value.toString().charAt(0));
+                    } else if (isNotNavigationPropertyType(field.getType())) {
+                        field.set(object, value);
+                    } else {
+                        field.set(object, setObjectFields(fieldsValues, field.getType()));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return object;
     }
 
     private static boolean isNotNavigationPropertyType(Class<?> classType) {
